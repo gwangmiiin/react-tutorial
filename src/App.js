0c0294bb951e1a0c8238829d2c82f24011f684ed
gwangmiiin <0424kgm@gmail.com>
2022-11-29 16:24:37 +0900

    jsx규칙

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from "react";
 import Hello from "./Hello";
+import './App.css'
 // import Wrapper from "./Wrapper";
 
 function App() {
+  const name = 'gwangmin';
+  const style = {
+    backgroundColor : 'black',
+    color : 'aqua',
+    fontSize : 24,
+    padding: '1rem'
+  };
   return (
-    <div>
+    <>
+    {/* 주석 사용 */}
       <Hello/>
-      <Hello/>
-      <Hello/>
-    </div>
+      <div
+      // 주석사용
+      style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
   );
 }
 
