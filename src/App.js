91d4b2c16ffbabb483a08d3ef9d346a7685037cb
gwangmiiin <0424kgm@gmail.com>
2023-01-31 14:28:18 +0900

    조건부 랜더링

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Wrapper from "./Wrapper";
 function App() {
   return (
     <Wrapper>
-      <Hello name = 'react' color = 'red'/>
+      <Hello name = 'react' color = 'red' isSpecial = {true} />  {/* 값 설정을 해주지 않으면 T로 인식 */}
       <Hello color = 'pink'/>
     </Wrapper>
   );
--- src/Hello.js
+++ src/Hello.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-function Hello({color , name}) {
-  return <div style={{
+function Hello({color , name, isSpecial }) {
+  return (
+    <div style={{
     color
-  }}>안녕하세요 {name}</div>;
+    }}>
+      {isSpecial && <b>*</b>} 
+      {/* 내용이 바뀌어야 할 때는 삼항연산자 사용 , 단순히 보여지고 숨기는 부분은 &연산자 사용*/}
+      안녕하세요 {name}
+    </div>
+  );
 }
 
 Hello.defaultProps = {
