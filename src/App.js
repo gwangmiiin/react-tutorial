28ef8d6e73575e7993c2a345eebecb19354b7886
gwangmiiin <0424kgm@gmail.com>
2023-02-06 14:50:28 +0900

    배열에 항목 수정하기

--- src/App.js
+++ src/App.js
@@ -22,17 +22,20 @@ function App() {
     {
       id: 1,
       username: 'gwangmiiin', 
-      email: '0424kgm@gmail.com'
+      email: '0424kgm@gmail.com',
+      active: true
     },
     {
       id: 2,
       username: 'tester',
-      email: 'tester@example.com'
+      email: 'tester@example.com',
+      active: false
     },
     {
       id: 3,
       username: 'liz',
-      email: 'liz@example.com'
+      email: 'liz@example.com',
+      active: false
     }
   ]);
 
@@ -62,6 +65,14 @@ function App() {
     setUsers(users.filter(user => user.id !== id));
   };
 
+  const onToggle = id => {
+    setUsers(
+      users.map(user =>
+        user.id === id ? { ...user, active: !user.active } : user
+      )
+    );
+  };
+
   return (
     <>
       <CreateUser 
@@ -70,7 +81,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove}/>
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
     </>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -1,21 +1,36 @@
 import React from "react";
 
-function User({ user, onRemove }) {
-  const { username, email, id } = user;
+function User({ user, onRemove, onToggle }) {
+  const { username, email, id, active } = user;
   return (
     <div>
-      <b>{username}</b> <span>({email})</span>
+      <b style={{
+        color : active ? 'green' : 'black',
+        cursor : 'pointer'
+      }}
+        onClick={() => onToggle(id)}
+      >
+        {username}
+      </b> 
+      &nbsp;
+      <span>({email})</span>
       <button onClick={() => onRemove(id)}>삭제</button>
       {/* 이 버튼이 눌렸을때 이러한 함수 호출, id가 특정 값인것을 삭제하겠다. 함수를 넣어주지 않으면 랜더링 되는 시점에서 모두 삭제됨 */}
     </div>
   );
 }
 
-function UserList({ users, onRemove }) {
+function UserList({ users, onRemove, onToggle }) {
   return (
     <div>
-      {users.map(user => (
-        <User user={user} key={user.id} onRemove={onRemove} />
+      {users.map(
+        user => (
+        <User 
+          user={user} 
+          key={user.id} 
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
