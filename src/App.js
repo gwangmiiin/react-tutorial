e47e40f1c2de321e4d6dc2b61c0bf345495e47a1
gwangmiiin <0424kgm@gmail.com>
2023-02-06 14:19:13 +0900

    배열에 항목 추가하기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, {useRef} from "react";
+import React, {useRef, useState } from "react";
+import CreateUser from "./CreateUser";
 import UserList from "./UserList";
 
 
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+  const { username, email } =inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  }
+
+  const [users, setUsers] = useState([
     {
       id: 1,
-      username: 'gwangmiiin',
+      username: 'gwangmiiin', 
       email: '0424kgm@gmail.com'
     },
     {
@@ -20,18 +34,38 @@ function App() {
       username: 'liz',
       email: 'liz@example.com'
     }
-  ];
+  ]);
 
   const nextId = useRef(4);
   const onCreate = () => {
     // 나중에 구현 할 배열에 항목 추가하는 로직
     // ...
 
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    setUsers([...users, user]);
+    // setUsers(users.concat(user)); 사용가능 즉 배열에 항목 추가하는 방법은 두가지
+
+    setInputs({
+      username: '',
+      email: ''
+    });
     nextId.current += 1;
   };
 
   return (
-    <UserList users={users}/>
+    <>
+      <CreateUser 
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users}/>
+    </>
   );
 }
 
